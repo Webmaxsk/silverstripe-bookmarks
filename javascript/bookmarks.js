710778aa5ef038a0083788cb828653a0baf1e1bf
rasstislav <brandobur@webmax.sk>
2017-08-01 14:03:45 +0200

    minor refactoring

--- javascript/bookmarks.js
+++ javascript/bookmarks.js
@@ -1,27 +1,25 @@
-function init_bookmarks() {
-	$('.showBookmarks').magnificPopup({
-		type: 'ajax',
-		closeBtnInside: true,
-		fixedContentPos: true,
-		callbacks: {
-			open: function() {
-				return $('html').addClass('popup-opened');
-			},
-			close: function() {
-				return $('html').removeClass('popup-opened');
-			}
-		},
-		tClose: ss.i18n._t('Bookmarks.CLOSE', 'Close (Esc)'),
-		tLoading: ss.i18n._t('Bookmarks.LOADING', 'Loading...')
-	});
+$('.showBookmarks').magnificPopup({
+  type: 'ajax',
+  closeBtnInside: true,
+  fixedContentPos: true,
+  callbacks: {
+    open: function() {
+      return $('html').addClass('popup-opened');
+    },
+    close: function() {
+      return $('html').removeClass('popup-opened');
+    }
+  },
+  tClose: ss.i18n._t('Bookmarks.CLOSE', 'Close (Esc)'),
+  tLoading: ss.i18n._t('Bookmarks.LOADING', 'Loading...')
+});
 
-	$(document).on('click', '#member_bookmarks .ajax-popup-link', function() {
-		$.magnificPopup.open({
-			items: {
-				src: $(this).attr('href')
-			}
-		});
+$(document).on('click', '#member_bookmarks .ajax-popup-link', function() {
+  $.magnificPopup.open({
+    items: {
+      src: $(this).attr('href')
+    }
+  });
 
-		return false;
-	});
-}
+  return false;
+});
